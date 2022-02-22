621212cdc57eb48ba53add9f66247b93db4ecb4d
dongwook kim <kyaol4@gmail.com>
2022-02-22 23:50:47 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :game do
+    resources :characher, only: [:new, :create]
+  end
+  resources :characher, only: [:destroy]
 end
